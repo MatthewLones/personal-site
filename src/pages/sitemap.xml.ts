6822b016c0d231acdb194d7fde3e015a9049c2e7
MatthewLones <23hdv1@queensu.ca>
2026-02-20 16:27:36 -0500

    cloudflare updates

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,16 @@
+export const prerender = true;
+
+export function GET() {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.mattlones.com/</loc>
+    <changefreq>weekly</changefreq>
+    <priority>1.0</priority>
+  </url>
+</urlset>`;
+
+  return new Response(sitemap, {
+    headers: { 'Content-Type': 'application/xml' },
+  });
+}
